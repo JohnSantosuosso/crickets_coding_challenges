294302b90c469af2ad7f4cde1318c1b6d35e6370
John Santosuosso <john.santosuosso1@gmail.com>
2024-02-01 15:30:08 -0500

    add line and word count

---
+++ wc_tool/ccwc.rb
@@ -0,0 +1,44 @@
+require 'optparse'
+
+options = {}
+
+
+OptionParser.new do |opts|
+  opts.on("-c", "--count", "Count bytes in file") do
+    options[:count] = true
+  end
+
+  opts.on("-l", "--lines", "Count lines in the specified file") do
+    options[:lines] = true
+  end
+
+  opts.on("-w", "--lines", "Count words in the specified file") do
+    options[:words] = true
+  end
+end.parse!
+  
+  
+file_name = ARGV[0]
+
+if file_name.nil?
+  puts "Please provide a file name as a command line argument."
+else
+  if File.exist?(file_name)
+    case 
+    when options[:count]
+      byte_count = File.size(file_name)
+      puts "#{byte_count} #{file_name}"
+    when options[:lines]
+      line_count = File.foreach(file_name).count
+      puts "#{line_count} #{file_name}"
+    when options[:words]
+      word_count = File.read(file_name).split(/\s+/).count
+      puts "#{word_count} #{file_name}"
+    else
+      puts "Invalid option"
+    end
+  else
+    puts "File not found"
+  end
+end
+
